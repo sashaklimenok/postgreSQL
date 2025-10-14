0690ccdaeebcd11d7c1fc7dda85aa87d3e773ac6
Aliaksandr <aliaksandr_klimianok@epam.com>
2025-10-14 17:42:13 +0300

    add simple sql

---
+++ head_first/greg.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE greg_list;
+
+CREATE TABLE doughnut_list (
+    doughnut_name VARCHAR(10),
+    doughnut_type VARCHAR(6)
+);
---
+++ queries/publisher-book.sql
@@ -0,0 +1,38 @@
+CREATE TABLE publisher (
+    publisher_id SERIAL NOT NULL,
+    org_name CHARACTER varying(32) NOT NULL,
+    address TEXT NOT NULL,
+    CONSTRAINT pk_publisher_id PRIMARY KEY (publisher_id)
+);
+
+CREATE TABLE book (
+    book_id SERIAL NOT NULL,
+    title TEXT NOT NULL,
+    isbn CHARACTER varying(32) NOT NULL,
+    fk_publisher_id SERIAL NOT NULL,
+    CONSTRAINT fk_publisher_id FOREIGN KEY (fk_publisher_id) REFERENCES publisher (publisher_id) ON DELETE CASCADE -- If publisher deleted, delete their books too
+    ON UPDATE CASCADE, -- If publisher_id changes, update books too
+    CONSTRAINT pk_book_id PRIMARY KEY (book_id)
+);
+
+ALTER TABLE book
+ADD CONSTRAINT fk_book_publisher FOREIGN KEY (fk_publisher_id) REFERENCES publisher (publisher_id)
+
+INSERT INTO
+    book
+VALUES (1, 'Book 1', '12345678', 2),
+    (2, 'Book 2', '87654321', 2),
+    (3, 'Book 3', '98765432', 1);
+
+INSERT INTO
+    publisher
+VALUES (1, 'Publisher 1', 'BY'),
+    (2, 'Publisher 2', 'BY');
+
+SELECT * FROM book;
+
+SELECT * FROM publisher;
+
+DROP TABLE publisher;
+
+DROP TABLE book;
