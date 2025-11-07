cbd8f820b6709b8ebbcef8c2777eea33314ad7af
Aliaksandr <aliaksandr_klimianok@epam.com>
2025-11-07 13:39:23 +0300

    add module-5

---
+++ northwind/5.DDL/DDl.sql
@@ -0,0 +1,14 @@
+-- Data Definition Language
+
+-- CREATE TABLE table_name (...columns) VALUES (...values_column)
+
+-- ALTER TABLE table_name
+-- ALTER TABLE table_name ADD COLUMN column_name data_typ
+-- ALTER TABLE table_name RENAME TO new_table_name
+-- ALTER TABLE table_name RENAME old_name_column TO new_column_name
+-- ALTER TABLE table_name DROP COLUMN column_name
+-- ALTER TABLE table_name ALTER_COLUMN column_name SET DATA TYPE
+
+-- DROP TABLE table_name
+
+-- TRUNCATE TABLE table_name
